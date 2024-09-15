941312a1172dd162062243629b8a789b62a3f003
taizou <cactusgunman@gmail.com>
2024-09-15 02:33:02 +0100

    missed this

--- src/libVf.h
+++ src/libVf.h
@@ -11,7 +11,6 @@ u8 DoSramWriteAndRead(u16 writeLocation,u8 writeValue, u16 readLocation);
 u8 DoSramRead(u16 readLocation);
 
 void DumpRom(u8* data, u32 startingOffset, u32 memSize);
-void DumpRom16(u16* data, u32 startingOffset, u32 memSize);
 void DumpRom32(u32* data, u32 startingOffset, u32 memSize);
 u32 DumpRom32Yj(u32* data, u32 startingOffset, u32 memSize, const u32 skips[16], int skipBlockStart, int skipBlockEnd);
 void DumpSram(u8* data);
